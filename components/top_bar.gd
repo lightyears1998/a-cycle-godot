8b11df6bcc641d1a0765054b7ad9a658bb1780f3
lightyears1998 <lightyears1998@hotmail.com>
2022-09-08 16:15:30 +0800

    Add a before-go-back hook to TopBar

--- components/top_bar.gd
+++ components/top_bar.gd
@@ -1,4 +1,10 @@
 extends HBoxContainer
 
-func _on_go_back_button_pressed():
-	Screens.go_back_otherwise_quit()
+@export var before_go_back_hook: Callable = func(): return true;
+
+func _on_go_back_button_pressed() -> void:
+	var should_go_back = true
+	if not before_go_back_hook.is_null():
+		should_go_back = await before_go_back_hook.call()
+	if should_go_back:
+		Screens.go_back_otherwise_quit()
