469d8395cbc763503990113ef263ed724579c00f
lightyears1998 <lightyears1998@hotmail.com>
2022-09-08 16:13:46 +0800

    Implement DatetimeEdit

--- components/datetime_edit.gd
+++ components/datetime_edit.gd
@@ -1,7 +1,9 @@
 @tool
 extends HBoxContainer
 
-@export var datetime_dict = {
+signal datetime_dict_changed(datetime_dict)
+
+var _datetime_dict: Dictionary = {
 	"year": 0,
 	"month": 1,
 	"day": 1,
@@ -10,31 +12,41 @@ extends HBoxContainer
 	"second": 0
 }
 
+@export var datetime_dict: Dictionary:
+	get:
+		return _datetime_dict
+	set(value):
+		_datetime_dict = value
+		_update_ui()
+
 @onready var datetime_label = %DatetimeLabel as Label
 @onready var pick_button = %PickButton as Button
 @onready var clear_button = %ClearButton as Button
 @onready var datetime_picker_popup = %DatetimePickerPopup as Popup
 
-func _update_ui():
+func _update_ui() -> void:
+	if not is_inside_tree():
+		return
 	if datetime_dict:
 		datetime_label.text = Time.get_datetime_string_from_datetime_dict(datetime_dict, true)
 	else:
 		datetime_label.text = "No date"
 
-func clear_datetime():
-	datetime_dict = null
+func clear_datetime() -> void:
+	datetime_dict = {}
 
-func _ready():
+func _ready() -> void:
 	_update_ui()
 
-func _on_pick_button_pressed():
+func _on_pick_button_pressed() -> void:
 	datetime_picker_popup.datetime_dict = datetime_dict
 	datetime_picker_popup.popup_centered()
 
-func _on_clear_button_pressed():
+func _on_clear_button_pressed() -> void:
 	clear_datetime()
 	_update_ui()
 
-func _on_datetime_picker_popup_datetime_changed(new_datetime_dict):
+func _on_datetime_picker_popup_datetime_changed(new_datetime_dict) -> void:
 	datetime_dict = new_datetime_dict
+	datetime_dict_changed.emit(datetime_dict)
 	_update_ui()
--- scripts/globals/datetime.gd
+++ scripts/globals/datetime.gd
@@ -1,8 +1,15 @@
 extends Node
 
+## Get local datetime dict from system clock.
+func get_datetime_dict() -> Dictionary:
+	return Time.get_datetime_dict_from_system(false)
+
+## Get ISO 8601 timestamp with timezone information.
+## This timestamp is compatible with Node.js default Date converting from JSON.
 func get_timestamp() -> String:
 	return Time.get_datetime_string_from_system(true) + "Z"
 
+## Get unix time in integer format.
 func get_unix_time() -> int:
 	return int(Time.get_unix_time_from_system())
 
@@ -10,3 +17,28 @@ func get_unix_time_from_timestamp(timestamp: String) -> int:
 	timestamp = timestamp.trim_suffix("Z")
 	var datetime_dict = Time.get_datetime_dict_from_datetime_string(timestamp, false)
 	return Time.get_unix_time_from_datetime_dict(datetime_dict)
+
+func _append_unix_time_offset_to_datetime_dict(datetime_dict: Dictionary, offset: int) -> Dictionary:
+	var unix_time: int = Time.get_unix_time_from_datetime_dict(datetime_dict)
+	unix_time += offset
+	return Time.get_datetime_dict_from_unix_time(unix_time)
+
+func get_next_day(datetime_dict: Dictionary) -> Dictionary:
+	return _append_unix_time_offset_to_datetime_dict(datetime_dict, 60 * 60 * 24)
+
+func get_previous_day(datetime_dict: Dictionary) -> Dictionary:
+	return _append_unix_time_offset_to_datetime_dict(datetime_dict, -60 * 60 * 24)
+
+func get_the_beginning_of_the_day(datetime_dict: Dictionary) -> Dictionary:
+	var new_dict := datetime_dict.duplicate(true)
+	new_dict["hour"] = 0
+	new_dict["minute"] = 0
+	new_dict["second"] = 0
+	return new_dict
+
+func get_the_end_of_the_day(datetime_dict: Dictionary) -> Dictionary:
+	var new_dict := datetime_dict.duplicate(true)
+	new_dict["hour"] = 23
+	new_dict["minute"] = 59
+	new_dict["second"] = 59
+	return new_dict
