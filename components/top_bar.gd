11b72dbbba39499cf878a967e51d268b6cdbe86a
lightyears1998 <lightyears1998@hotmail.com>
2022-09-10 15:42:37 +0800

    Take a note on linter false positive

--- components/top_bar.gd
+++ components/top_bar.gd
@@ -5,6 +5,8 @@ extends HBoxContainer
 func _on_go_back_button_pressed() -> void:
 	var should_go_back = true
 	if not before_go_back_hook.is_null():
+		# Linter has false positive `REDUNDANT_AWAIT` warning
+		# We must `await` here because `before_go_back_hook` could be a "coroutine" callable
 		should_go_back = await before_go_back_hook.call()
 	if should_go_back:
 		Screens.go_back_otherwise_quit()
