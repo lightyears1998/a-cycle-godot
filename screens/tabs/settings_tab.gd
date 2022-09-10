677702fb9afd2eaf81d8edd5596693c1cdd4e1f8
lightyears1998 <lightyears1998@hotmail.com>
2022-09-10 09:04:22 +0800

    Mount LogcatScreen

--- screens/tabs/settings_tab.gd
+++ screens/tabs/settings_tab.gd
@@ -2,6 +2,7 @@ extends VBoxContainer
 
 const ServerManagementScreen = preload("res://screens/server_management_screen.tscn")
 const ExportScreen = preload("res://screens/export_screen.tscn")
+const LogcatScreen = preload("res://screens/logcat_screen.tscn")
 
 func _on_manage_servers_button_pressed():
 	Screens.go_to(ServerManagementScreen.instantiate())
@@ -10,4 +11,4 @@ func _on_export_button_pressed():
 	Screens.go_to(ExportScreen.instantiate())
 
 func _on_logcat_button_pressed():
-	pass # Not implemented yet.
+	Screens.go_to(LogcatScreen.instantiate())
