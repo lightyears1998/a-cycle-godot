53a6671bc2b86f2de127cc13ee61ceb8cd6c43be
lightyears1998 <lightyears1998@hotmail.com>
2022-09-10 22:36:04 +0800

    Get ready for sync

--- scripts/resources/sync_server_config.gd
+++ scripts/resources/sync_server_config.gd
@@ -9,7 +9,7 @@ class_name SyncServerConfig
 @export var password := "" # TODO storaging hash instead of plain text to protect privacy.
 @export var use_tls := false
 
-@export var user_id := "0"
+@export var user_id := ""
 @export var token := ""
 
 func get_restful_url() -> String:
@@ -20,5 +20,5 @@ func get_socket_url() -> String:
 	var schema = "wss" if use_tls else "ws"
 	return schema + "://" + host + ":" + str(ws_port) + "/" + path
 
-func compose_identifier() -> String:
+func get_identifier() -> String:
 	return "%s@%s" % [username, host]
--- scripts/services/sync.gd
+++ scripts/services/sync.gd
@@ -9,8 +9,16 @@ func _ready() -> void:
 func _exit_tree() -> void:
 	http_request.queue_free()
 
+func prepare_sync(config: SyncServerConfig) -> void:
+	Logcat.info(config.get_identifier() + " Preparing Sync.")
+	if config.user_id.is_empty():
+		await get_user_id(config)
+	if config.token.is_empty():
+		await get_user_token(config)
+	Logcat.info(config.get_identifier() + " Preparation finished.")
+
 func get_user_id(config: SyncServerConfig) -> String:
-	Logcat.info("Requesting user id for " + config.compose_identifier() + ".")
+	Logcat.info(config.get_identifier() + " Requesting user id...")
 
 	var url = config.get_restful_url()
 	url += '/users/?username=' + config.username.uri_encode()
@@ -30,11 +38,11 @@ func get_user_id(config: SyncServerConfig) -> String:
 	var user = payload["user"]
 	var user_id = user["id"]
 	config.user_id = user_id
-	Logcat.info("Updated user id (" + str(config.user_id) + ") for " +  config.compose_identifier() + ".")
+	Logcat.info(config.get_identifier() + " Updated user id: %s." % str(config.user_id))
 	return user_id
 
 func get_user_token(config: SyncServerConfig) -> String:
-	Logcat.info("Requesting user token for " + config.compose_identifier() + ".")
+	Logcat.info(config.get_identifier() + " Requesting user token for %s." % config.user_id)
 
 	var url = config.get_restful_url()
 	url += '/users/:userId/jwt-tokens'.replace(":userId", str(config.user_id))
@@ -58,6 +66,6 @@ func get_user_token(config: SyncServerConfig) -> String:
 		Logcat.error("Failed to get user token.")
 		return ""
 
-	Logcat.info("Accquired user token %s for " % token + config.compose_identifier() + ".")
+	Logcat.info(config.get_identifier() + " Accquired user token %s for %s." % [token, config.user_id])
 	config.token = token
 	return token
