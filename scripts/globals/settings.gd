64287f381a700d7295fa72ab1ceac9df2290aa43
lightyears1998 <lightyears1998@hotmail.com>
2022-09-11 10:38:55 +0800

    Prevent loading non-existent file

--- scripts/globals/settings.gd
+++ scripts/globals/settings.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var app_config := AppConfig.new()
-var save_path := "user://settings{suffix}.tres".format({"suffix": _get_file_suffix()})
+var save_path := "user://app_config{suffix}.tres".format({"suffix": _get_file_suffix()})
 var time_zone := Time.get_time_zone_from_system()
 var db_path := "user://database{suffix}.sqlite3".format({"suffix": _get_file_suffix()})
 
@@ -14,11 +14,15 @@ func _get_file_suffix() -> String:
 func _save():
 	ResourceSaver.save(app_config, save_path)
 
+func _is_app_config_exists():
+	return File.new().file_exists(save_path)
+
 func _load():
-	var loaded_config = ResourceLoader.load(save_path, "AppConfig")
-	if loaded_config:
-		app_config = loaded_config
-	app_config._normalize()
+	if _is_app_config_exists():
+		var loaded_config = ResourceLoader.load(save_path, "AppConfig")
+		if loaded_config:
+			app_config = loaded_config
+		app_config._normalize()
 
 func _ready():
 	_load()
