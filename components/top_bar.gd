f844f6eb2a265a3ea27cb4b902bea3d763ca676d
lightyears1998 <lightyears1998@hotmail.com>
2022-09-13 10:40:48 +0800

    Lint

--- components/top_bar.gd
+++ components/top_bar.gd
@@ -7,6 +7,7 @@ func _on_go_back_button_pressed() -> void:
 	if not before_go_back_hook.is_null():
 		# Linter reports a false positive `REDUNDANT_AWAIT` warning
 		# We must `await` here because `before_go_back_hook` could be a "coroutine" callable
+		@warning_ignore(redundant_await)
 		should_go_back = await before_go_back_hook.call()
 	if should_go_back:
 		Screens.go_back_otherwise_quit()
