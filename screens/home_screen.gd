06b09b12aacad46ca9e0fe4210ca91b3b2ca440c
lightyears1998 <lightyears1998@hotmail.com>
2022-09-14 13:31:38 +0800

    Setup placeholder for future tabs

--- screens/home_screen.gd
+++ screens/home_screen.gd
@@ -2,6 +2,10 @@ extends MarginContainer
 
 @onready var tab_container = %TabContainer
 @onready var focus_tab = %FocusTab
+@onready var activity_tab = %ActivityTab
+@onready var todo_list_tab = %TodoListTab
+@onready var ledger_tab = %LedgerTab
+@onready var health_tab = %HealthTab
 @onready var diary_tab = %DiaryTab
 @onready var settings_tab = %SettingsTab
 
@@ -17,6 +21,18 @@ func _ready() -> void:
 
 func _on_focus_button_pressed() -> void:
 	_show_tab(focus_tab)
+
+func _on_activity_button_pressed() -> void:
+	_show_tab(activity_tab)
+
+func _on_todo_list_button_pressed() -> void:
+	_show_tab(todo_list_tab)
+
+func _on_ledger_button_pressed() -> void:
+	_show_tab(ledger_tab)
+
+func _on_health_button_pressed() -> void:
+	_show_tab(health_tab)
 
 func _on_diary_button_pressed() -> void:
 	_show_tab(diary_tab)
