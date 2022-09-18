94b40366b8251a5068b6133f505669cb68da915c
lightyears1998 <lightyears1998@hotmail.com>
2022-09-18 16:41:53 +0800

    Add `_on_screen_destroy` screen life cycle method

--- scripts/globals/screens.gd
+++ scripts/globals/screens.gd
@@ -1,20 +1,22 @@
 extends Node
 class_name ScreensSingleton
 
-var go_back_stack: Array[Node] = []
+var _go_back_stack: Array[Node] = []
 
 func _get_current_screen() -> Node:
 	return $/root.get_children()[$/root.get_child_count() - 1]
 
 func _change_screen_to(new_screen: Node) -> Node:
 	var old_screen = _get_current_screen()
+	if old_screen.has_method("_on_screen_destroy"):
+		old_screen._on_screen_destroy()
 	$/root.remove_child(old_screen)
 	$/root.add_child(new_screen)
 	return old_screen
 
 func go_back() -> bool:
-	if len(go_back_stack) > 0:
-		var previous_screen = go_back_stack.pop_back()
+	if len(_go_back_stack) > 0:
+		var previous_screen = _go_back_stack.pop_back()
 		var replaced_screen = _change_screen_to(previous_screen)
 		replaced_screen.queue_free()
 		if previous_screen.has_method("_on_screen_resume"):
@@ -29,18 +31,24 @@ func go_back_otherwise_quit() -> void:
 
 func go_to(next_screen: Node) -> void:
 	var replaced_screen = _change_screen_to(next_screen)
-	go_back_stack.push_back(replaced_screen)
+	_go_back_stack.push_back(replaced_screen)
 
-func free_nodes_in_go_back_stack() -> void:
+func toggle_always_on_top() -> void:
+	const key = "display/window/size/always_on_top"
+	var always_on_top_enabled = !ProjectSettings.get_setting(key)
+	DisplayServer.window_set_flag(Window.FLAG_ALWAYS_ON_TOP, always_on_top_enabled)
+	ProjectSettings.set_setting(key, always_on_top_enabled)
+
+func _free_nodes_in_go_back_stack() -> void:
 	while true:
-		var node = go_back_stack.pop_back()
+		var node = _go_back_stack.pop_back()
 		if node:
 			node.queue_free()
 		else:
 			break
 
 func _exit_tree() -> void:
-	free_nodes_in_go_back_stack()
+	_free_nodes_in_go_back_stack()
 
 func _notification(what):
 	match what:
