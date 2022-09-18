a36055ae20c72c7dd021a8b0e79a1618a9bb0d6e
lightyears1998 <lightyears1998@hotmail.com>
2022-09-18 16:42:14 +0800

    Add database schema

--- scripts/globals/database.gd
+++ scripts/globals/database.gd
@@ -1,16 +1,51 @@
 extends Node
 class_name DatabaseSingleton
 
+const CURRENT_DATABASE_SCHEMA := 1
+
 var db: SQLite = SQLite.new()
 
-func _init() -> void:
-	_open_db()
-	_create_table()
+var Entry = EntryRepository.new()
+var EntryHistory = EntryHistoryRepository.new()
+var Diary = DiaryRepository.new()
+var TodoItem = TodoItemRepository.new()
 
-func _open_db():
+func _init() -> void:
+	var ok := _open_db()
+	if ok:
+		_maintenance()
+		_create_table()
+
+func _open_db() -> bool:
 	db.path = Settings.db_path
 	db.verbosity_level = SQLite.NORMAL if not Settings.is_dev_env() else SQLite.VERY_VERBOSE
-	db.open_db()
+	return db.open_db()
+
+func _maintenance():
+	db.create_table("metadata", {
+		"key": { "data_type": "text", "primary_key": true },
+		"value": { "data_type": "text", "not_null": true },
+	})
+	var schema := _get_or_create_schema_version()
+	Logcat.info("Database schema version: %d." % schema)
+	if schema != CURRENT_DATABASE_SCHEMA:
+		_convert_schema(schema)
+
+func _get_or_create_schema_version() -> int:
+	var ok := db.query("select key, value from metadata where key='schema'")
+	if !ok:
+		return 0
+
+	var result = db.query_result.duplicate(true)
+	if len(result) == 0:
+		db.query("insert into metadata (key, value) values ('schema', %d)" % CURRENT_DATABASE_SCHEMA)
+		return CURRENT_DATABASE_SCHEMA
+
+	var schema = str(result[0]).to_int()
+	return schema
+
+func _convert_schema(_old_schema: int) -> void:
+	pass # placeholder for future
 
 func _create_table():
 	db.create_table("entry", {
