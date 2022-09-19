79a37a7ee5b164693bb73a29e344a7be2616e144
lightyears <lightyears1998@hotmail.com>
2022-09-19 08:33:35 +0800

    Fix log displaying order

--- screens/logcat_screen.gd
+++ screens/logcat_screen.gd
@@ -14,11 +14,13 @@ func _get_logs_in_buffer():
 			_show_log(log_item.time, log_item.statement, log_item.level)
 		idx = (idx - 1 + Logcat.BUFFER_SIZE) % Logcat.BUFFER_SIZE
 
-func _show_log(time: Datetime, statement: String, level: String):
+func _show_log(time: Datetime, statement: String, level: String, prepend := false):
 	var label= Label.new()
 	label.text = time.to_iso_timestamp() + " " + level + "\n" + statement
 	label.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
 	log_container.add_child(label)
+	if prepend:
+		log_container.move_child(label, 0)
 
 func _on_log_logged(time: Datetime, statement: String, level: String):
 	_show_log(time, statement, level)
